0ea3fa33c2b381fa635b21c502a2f3c5f5baea2f
MeryAmun <chebesiawah@gmail.com>
2021-12-12 02:54:45 +0100

    created get all post route

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv').config()
 const connectDb = require('./db/connect')
+const postRoute = require('./routes/postRoutes.js')
 
 const app = express()
-
+//routes
+app.use('/posts', postRoute)
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', (req, res) => {
-  res.status(200).json({
-    msg: 'Welcome to memories lane',
-  })
-})
+app.use(cors)
 
-const port = 3500
+const port = process.env.PORT || 3500
 
 const startServer = async () => {
   try {
---
+++ controllers/postController.js
@@ -0,0 +1,5 @@
+const getAllPosts = (req, res, next) => {
+  res.send('Hello world')
+}
+
+module.exports = getAllPosts
