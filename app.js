97d6927ad7a8f733c0b201e3679ee25eca1cecf4
MeryAmun <chebesiawah@gmail.com>
2021-12-14 04:46:03 +0100

    created update post route

--- app.js
+++ app.js
@@ -7,11 +7,28 @@ const connectDb = require('./db/connect')
 const postRoute = require('./routes/postRoutes.js')
 
 const app = express()
+//wew must always place app.use(cors) middleware above the routes.
+//app.use(cors)
+//middleware
+//app.use(express.json())
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
+
 //routes
 app.use('/posts', postRoute)
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors)
 
 const port = process.env.PORT || 3500
 
--- controllers/postController.js
+++ controllers/postController.js
@@ -4,11 +4,20 @@ const asyncWrapper = require('../middleware/async')
 const getAllPosts = asyncWrapper(async (req, res, next) => {
   const posts = await Post.find({})
   if (posts) {
-    res.status(200).json({ posts })
+    res.status(200).json(posts)
   }
 })
 const createPost = asyncWrapper(async (req, res, next) => {
   const post = await Post.create(req.body)
   res.status(201).json({ post })
 })
-module.exports = { getAllPosts, createPost }
+
+const updatePost = asyncWrapper(async (req, res, next) => {
+  const { id: _id } = req.params
+  const post = req.body
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send('no post with that id')
+  const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true })
+  res.status(201).json({ updatedPost })
+})
+module.exports = { getAllPosts, createPost, updatePost }
